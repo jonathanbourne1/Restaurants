24eb71ea05fe6881f2f52d1e77209d21da553667
JonathanDevBourne <jonathan.hernandez.oliva@gmail.com>
2020-07-18 08:05:07 -0500

    working to auth new branch to login-firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 import Navigation from './navigation/Navigation';
+//firebase
+import auth from '@react-native-firebase/auth';
+
 const App = () => {
+  const user = auth().currentUser;
+  console.log(user);
+
   return <Navigation />;
 };
 
